1da74899dbd17bcd121487586707398a64a712c4
OlivierMedigue <olivier.medigue@expleogroup.com>
2021-07-27 14:53:40 +0200

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+const openFilter = document.querySelectorAll(".icon-down");
+const closeFilter = document.querySelectorAll(".icon-up");
+const ingredientSearch = document.getElementById("ingredient");
+const appareilSearch = document.getElementById("appareil");
+const ustensilesSearch = document.getElementById("ustensiles");
+
+
+
